4de8cc0816ef904d068737d91f79e22c5121f692
Kamran Choudhry <theblizzzzard@gmail.com>
2016-08-05 20:24:45 -0400

    Update textEditor.py

--- textEditor.py
+++ textEditor.py
@@ -7,24 +7,20 @@ from sys import argv
 def fetchFile(filename):
 
 	d = input("\nEdit file = 1  View file = 2\n -> ")
-
+	
 	if d == 1:
-
 		txt = open(filename, 'w')
 		txt.truncate()
 		msg = raw_input("\nWhat would you like to enter in the file:\n -> ")
 		txt.write(msg)
 		txt.close()
 		fetchFile(filename)
-
 	elif d == 2:
-
 		txt = open(filename, 'r')
 		i = txt.read()
 		print "\nFile Content\n------------\n" + i
 		txt.close()
 		fetchFile(filename)
-
 	else: sys.exit()
 
 def menu(repo, filename):
@@ -45,13 +41,13 @@ def init(script, user, filename, prompt):
 
 	print "\nProceed to main menu? y/n\n",
 	repo = raw_input(prompt)
-
+	
 	menu(repo, filename)
 
 if __name__ == '__main__':
 
 	script, user, filename = argv  #python textEditor.py David file.txt
 	prompt = ' -> '
-
+	
 	init(script, user, filename, prompt)
 
