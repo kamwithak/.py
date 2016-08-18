9e39081bef8ce26f20afd3f5b3ed172a107b8eb4
Kamran Choudhry <theblizzzzard@gmail.com>
2016-08-18 16:55:45 -0400

    Update game.py

--- game.py
+++ game.py
@@ -10,10 +10,14 @@ gray = (205,201,201)
 
 lead_x = 400
 lead_y = 400
+lead_x_change = 0
+lead_y_change = 0
+
+time = pygame.time.Clock()
 
 #bg = pygame.image.load("wallpaper.jpg")
 #size = (width, height) = bg.get_size()
-game_display = pygame.display.set_mode((800,600)) 
+game_display = pygame.display.set_mode((800,600))
 pygame.display.set_caption("test_game")
 
 game_exit = False
@@ -25,22 +29,26 @@ while not game_exit: #event loop
 		if event.type == pygame.QUIT: #break loop and exit pygame
 			game_exit = True
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				lead_x -= 15
-			if event.key == pygame.K_RIGHT:
-				lead_x += 15
-			if event.key == pygame.K_UP:
-				lead_y -= 15
-			if event.key == pygame.K_DOWN:
-				lead_y += 15
+		if event.type == pygame.KEYDOWN: # left, up -> '-' right, bottom -> '+'
+			if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+				lead_x_change = -15
+			if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+				lead_x_change = 15
+			if event.key == pygame.K_UP or event.key == pygame.K_w:
+				lead_y_change = -15
+			if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+				lead_y_change = 15
 
-		print(event) #print events 
+		print(event) #print events
 
 	#game_display.blit(bg, (0, 0))
+	lead_x += lead_x_change
+	lead_y += lead_y_change
 	game_display.fill(gray)
-	pygame.draw.rect(game_display, navy_blue, [lead_x,lead_y,19,19])
+	pygame.draw.rect(game_display, navy_blue, [lead_x,lead_y,20,20])
 	pygame.display.update()
+
+	time.tick(29) #fps
 
 pygame.quit()
 sys.exit()
