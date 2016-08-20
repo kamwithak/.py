f0e45cda0a9dde8933fcf476877fa8d12606749d
Kamran Choudhry <theblizzzzard@gmail.com>
2016-08-19 23:37:26 -0400

    Update game.py

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ time = pygame.time.Clock()
 
 #bg = pygame.image.load("wallpaper.jpg")
 #size = (width, height) = bg.get_size()
-game_display = pygame.display.set_mode((800,600))
+game_display = pygame.display.set_mode((1000,800))
 pygame.display.set_caption("test_game")
 
 game_exit = False
@@ -29,26 +29,45 @@ while not game_exit: #event loop
 		if event.type == pygame.QUIT: #break loop and exit pygame
 			game_exit = True
 
-		if event.type == pygame.KEYDOWN: # left, up -> '-' right, bottom -> '+'
-			if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+		if event.type == pygame.KEYDOWN: #left, up -> '-' right, bottom -> '+'
+			if event.key == pygame.K_LEFT:
 				lead_x_change = -15
-			if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+				lead_y_change = 0
+			elif event.key == pygame.K_RIGHT:
 				lead_x_change = 15
-			if event.key == pygame.K_UP or event.key == pygame.K_w:
+				lead_y_change = 0
+			elif event.key == pygame.K_UP:
 				lead_y_change = -15
-			if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+				lead_x_change = 0
+			elif event.key == pygame.K_DOWN:
 				lead_y_change = 15
+				lead_x_change = 0
+
+			elif event.key == pygame.K_q: #q to exit program
+				pygame.quit()
+				sys.exit()
 
 		print(event) #print events
+
+#instantaneously stop motion for a frame (hold down effect)
+
+#		if event.type == pygame.KEYUP: 
+#			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+#				lead_x_change = 0
+#			elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+#				lead_y_change = 0
+
+	if lead_x > 1000 or lead_x < 0 or lead_y > 800 or lead_y < 0: #boundaries 
+		game_exit = True
 
 	#game_display.blit(bg, (0, 0))
 	lead_x += lead_x_change
 	lead_y += lead_y_change
 	game_display.fill(gray)
-	pygame.draw.rect(game_display, navy_blue, [lead_x,lead_y,20,20])
+	pygame.draw.rect(game_display, navy_blue, [lead_x,lead_y,30,30])
 	pygame.display.update()
 
-	time.tick(29) #fps
+	time.tick(60) #fps
 
 pygame.quit()
 sys.exit()
