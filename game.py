83e43a532ecea35cfc645d2d8186adb912287d4c
Kamran Choudhry <theblizzzzard@gmail.com>
2016-08-24 23:05:07 -0400

    Update game.py

--- game.py
+++ game.py
@@ -17,32 +17,49 @@ display_height = 600
 
 clock = pygame.time.Clock()
 
-#bg = pygame.image.load("wallpaper.jpg")
-#size = (width, height) = bg.get_size()
 game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("snake_game")
 
-block_size = 15
-block_movement = 10 #keep block_movement = 10 for grid effect 
-FPS = 19
+block_size = 20 #used for snake size
+apple_thickness = 22 #used for apple size
+block_movement = 10 #snake movement, keep block_movement = 10 for grid effect 
+FPS = 20
 
-font_center = pygame.font.SysFont(None,30)
-font_for_counter = pygame.font.SysFont(None,40)
+small_font = pygame.font.SysFont("comicsansms", 25)
+medium_font = pygame.font.SysFont("comicsansms", 35)
+large_font = pygame.font.SysFont("comicsansms", 45)
 
-def counter_displayer(counter):
+def ingame_counter_displayer(counter):
 
-	counter_text = font_for_counter.render('SCORE: ' + str(counter), True, gray)
-	game_display.blit(counter_text, [20, 550])
+	ingame_counter_text = small_font.render('SCORE: ' + str(counter), True, white)
+	game_display.blit(ingame_counter_text, [20, 550])
+
+def final_counter_displayer(counter):
+
+	final_counter_text = small_font.render('FINAL SCORE: ' + str(counter), True, white)
+	game_display.blit(final_counter_text, [20, 550])
 
 def snake(block_size, snake_list):
 
 	for XnY in snake_list:
 		pygame.draw.rect(game_display, green, [XnY[0], XnY[1], block_size, block_size]) #drawing snake
 
-def message_to_center_of_screen(msg, color):
+def text_objects(text, color, size):
 
-	screen_text = font_center.render(msg, True, color)
-	game_display.blit(screen_text, [220, display_height/2]) #center the text, play with this *
+	if size == "small":
+		text_surface = small_font.render(text, True, color)
+	elif size == "medium":
+		text_surface = medium_font.render(text, True, color)
+	elif size == "large":
+		text_surface = large_font.render(text, True, color)
+
+	return text_surface, text_surface.get_rect()
+	
+def message_to_center(msg, color, y_displace=0, size="small"):
+
+	text_surf, text_rect = text_objects(msg, color, size)
+	text_rect.center = (display_width/2), (display_height/2) + y_displace
+	game_display.blit(text_surf, text_rect)
 
 def game_loop():
 
@@ -55,19 +72,22 @@ def game_loop():
 
 	game_exit = False
 	game_over = False
+	game_success = False
 
 	snake_list = []
-	snake_length = 7
+	snake_length = 1 #init snake length
 
-	randAppleX = round(random.randint(60, 500)/10)*10 #formula that rounds to nearest 10th for grid effect
-	randAppleY = round(random.randint(60, 500)/10)*10 #generates random coordinate for initial apple 
+	randAppleX = round(random.randint(60, 500))#/10)*10 #formula that rounds to nearest 10th for grid effect (currently commented)
+	randAppleY = round(random.randint(60, 500))#/10)*10 #generates random coordinate for initial apple 
 
 	while not game_exit: #event loop
 
 		while game_over == True:
 
 			game_display.fill(black)
-			message_to_center_of_screen("You lose! Q to exit, or R to restart  :D", red)
+			message_to_center("Game over!", red, -60, "large")
+			message_to_center("Press Q to exit, or R to restart  :D", white, 0, "medium")
+			final_counter_displayer(counter)
 			pygame.display.update()
 
 			for event in pygame.event.get():
@@ -79,6 +99,27 @@ def game_loop():
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_q: #Q
 						game_over = False
+						game_exit = True
+					if event.key == pygame.K_r: #R
+						game_loop()
+
+		while game_success == True:
+
+			game_display.fill(black)
+			message_to_center("Successfully Completed!", green, -63, "large")
+			message_to_center("Press Q to exit, or R to restart  :D", white, 0, "medium")
+			final_counter_displayer(counter)
+			pygame.display.update()
+
+			for event in pygame.event.get():
+
+				if event.type == pygame.QUIT: #break loops and exit pygame
+					game_success = False
+					game_exit = True
+
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q: #Q
+						game_success = False
 						game_exit = True
 					if event.key == pygame.K_r: #R
 						game_loop()
@@ -110,7 +151,7 @@ def game_loop():
 
 			print(event) #print events
 
-	# ~~ instantaneously stop motion for a frame (hold down effect)
+	# ~~ (hold down effect)
 
 	# 		if event.type == pygame.KEYUP: 
 	# 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -118,21 +159,36 @@ def game_loop():
 	# 			elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
 	# 				lead_y_change = 0
 
+		# ~~ snake and apple meet 
+
+		# if lead_x >= randAppleX and lead_x <= randAppleX + apple_thickness:
+		# 	if lead_y >= randAppleY and lead_y <= randAppleY + apple_thickness:
+		# 		randAppleX = round(random.randint(60, 500))#/10)*10 #formula that rounds to nearest 10th for grid effect (currently commented)
+		# 		randAppleY = round(random.randint(60, 500))#/10)*10 #also regenerates coordinates for new apple
+		# 		snake_length += 1 #increase snake length
+		# 		counter += 1 #adjust score 
+
+		if lead_x > randAppleX and lead_x < randAppleX + apple_thickness or lead_x + block_size > randAppleX and lead_x + block_size < randAppleX + apple_thickness:
+			if lead_y > randAppleY and lead_y < randAppleY + apple_thickness or lead_y + block_size > randAppleY and lead_y + block_size < randAppleY + apple_thickness:
+		 		randAppleX = round(random.randint(60, 500))#/10)*10 #formula that rounds to nearest 10th for grid effect (currently commented)
+		 		randAppleY = round(random.randint(60, 500))#/10)*10 #regenerates coordinates for new apple
+		 		snake_length += 1 #increase snake length
+		 		counter += 1 #adjust score
+
+		if counter == 5:
+			game_success = True
+
 		if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0: #boundaries for game screen
 			game_over = True
-
-		if lead_x == randAppleX and lead_y == randAppleY:
-			randAppleX = round(random.randint(60, 500)/10)*10 #formula that rounds to nearest 10th for grid effect
-			randAppleY = round(random.randint(60, 500)/10)*10 #also regenerates coordinates for new apple
-			counter += 1 
 
 		#game_display.blit(bg, (0, 0))
 		lead_x += lead_x_change
 		lead_y += lead_y_change
 
 		game_display.fill(black)
-		counter_displayer(counter)
-		pygame.draw.rect(game_display, red, [randAppleX, randAppleY, block_size, block_size]) #drawing apple
+		ingame_counter_displayer(counter)
+		pygame.draw.rect(game_display, red, [randAppleX, randAppleY, apple_thickness, apple_thickness]) #drawing apple
+		
 		snake_head = []
 		snake_head.append(lead_x)
 		snake_head.append(lead_y)
@@ -141,11 +197,14 @@ def game_loop():
 		if len(snake_list) > snake_length: #line-effect for snake, disable condition if confused
 			del snake_list[0]
 
-		snake(block_size, snake_list)
+		for each_segment in snake_list[:-1]: #head of snake meets body of snake = game over
+			if each_segment == snake_head:
+				game_over = True
 
+		snake(block_size, snake_list)
+		clock.tick(FPS) #fps
 		pygame.display.update()
 
-		clock.tick(FPS) #fps
 
 	pygame.quit()
 	sys.exit()
@@ -153,4 +212,3 @@ def game_loop():
 if __name__ == "__main__":
 
 	game_loop()
-
