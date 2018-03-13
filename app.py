3c8ba49f1c196052442ae9930daf96884477fd3f
Kamran Choudhry <theblizzzzard@gmail.com>
2018-03-13 16:37:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # configure MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '885265Om.'
+app.config['MYSQL_PASSWORD'] = 'password1'
 app.config['MYSQL_DB'] = 'MYFLASKAPP'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
