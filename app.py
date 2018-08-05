ca4feddcbc6e304fa03432112f134c5d66518f0f
Kamran C <theblizzzzard@gmail.com>
2018-08-05 14:04:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# Flask-based backend for an encrypted (SHA256) user-registration/database platform that manages project repositories
-# Project repositories are stored using MySQL
+# Flask-based backend for an encrypted user-registration/database platform that manages project repositories
+# Database technology: SHA256 for encryption and MySQL for storing
 # Developed by Kamran Choudhry
 
 from flask import Flask, session, render_template, flash, redirect, url_for, session, logging, request
