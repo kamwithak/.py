0b2dfc048034dc310a352cfb2b8eae73e7e3225e
Kamran C <theblizzzzard@gmail.com>
2019-02-05 13:19:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Flask-based backend for an encrypted user-registration/database web platform that manages project repositories
+# Full stack application available upon request
 # ~~
 # Database Technologies: SHA256 for data encryption and MySQL for server-side storing
 # Developer: Kamran Choudhry
@@ -22,12 +23,12 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # initialize MySQL
 mysql = MySQL(app)
 
-# init
+# init page
 @app.route('/')
 def init():
     return render_template('init.html')
 
-# check if user is logged in
+# check if the user is currently logged in
 def is_logged_in(f):
 	@wraps(f)
 	def wrap(*args, **kwargs):
@@ -38,62 +39,53 @@ def is_logged_in(f):
 			return redirect(url_for('login'))
 	return wrap
 	
-# dashboard
+# dashboard page
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-	# create cursor
-	cur = mysql.connection.cursor()
-	# get articles
-	result = cur.execute("SELECT * FROM articles WHERE author = %s", [session['username']])
+	cur = mysql.connection.cursor()																	# create db cursor
+	result = cur.execute("SELECT * FROM articles WHERE author = %s", [session['username']])			# get articles
 	articles = cur.fetchall()
-	if result > 0: #if there is any rows
-		return render_template('dashboard.html', articles=articles)
+	if result > 0: 																		# if there are any rows
+		return render_template('dashboard.html', articles=articles)						# send data through to dashboard page		
 	else:
-		msg_red = "~ EMPTY DASHBOARD ~"
-		return render_template('dashboard.html', msg_red=msg_red)
-	# close connection
-	cur.close()
+		msg_red = "~ EMPTY DASHBOARD ~"													# there aren't any rows
+		return render_template('dashboard.html', msg_red=msg_red)						# send error signal to dashboard page
+	cur.close()																			# close db cursor
  
 # about
 @app.route('/about')
 def about():
-	return render_template('about.html')
+	return render_template('about.html')												# load about website page
 
 # articles
-@app.route('/repository')
+@app.route('/repository')								
 @is_logged_in
 def articles():
-	# create cursor
-	cur = mysql.connection.cursor()
-	# get articles
-	result = cur.execute("SELECT * FROM articles")
+	cur = mysql.connection.cursor()														# create db cursor
+	result = cur.execute("SELECT * FROM articles")										# get articles
 	articles = cur.fetchall()
-	if result > 0: #if there is any rows
+	if result > 0: 																		# if there are any rows
 		return render_template('articles.html', articles=articles)
 	else:
-		msg_red = "~ EMPTY REPOSITORY ~"
-		return render_template('articles.html', msg_red=msg_red)
-	# close connection
-	cur.close()
+		msg_red = "~ EMPTY REPOSITORY ~"								
+		return render_template('articles.html', msg_red=msg_red)						# WLOG
+	cur.close()																			# close db cursor
 
 # inside each article
 @app.route('/repository/<string:id>')
 @is_logged_in
 def article(id):
-	# create cursor
-	cur = mysql.connection.cursor()
-	# get articles then article
+	cur = mysql.connection.cursor()														# create db cursor
 	result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
 	article = cur.fetchone()
-	# close connection
-	cur.close()
-	return render_template("article.html", article=article)
+	cur.close()																			# close connection
+	return render_template("article.html", article=article)								# load database page
 
 # register form class
-class RegisterForm(Form):
-	name = StringField('Name', [validators.Length(min=1, max=50)])
-	username = StringField('Username', [validators.Length(min=4, max=25)])
+class RegisterForm(Form):																# lovely in python
+	name = StringField('Name', [validators.Length(min=1, max=50)])						
+	username = StringField('Username', [validators.Length(min=4, max=25)])				# following code reads like english
 	email = StringField('Email', [validators.Length(min=6, max=50)])
 	password = PasswordField('Password', [
 		validators.DataRequired(),
@@ -111,13 +103,9 @@ def register():
 		email = form.email.data
 		username = form.username.data
 		password = sha256_crypt.encrypt(str(form.password.data))
-		# create the cursor
 		cur = mysql.connection.cursor()
-		# execute query statement
 		cur.execute('INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)', (name, email, username, password))
-		# commit to database 
 		mysql.connection.commit()
-		# close connection
 		cur.close()
 		flash("~ SUCCESSFULLY REGISTERED ~", "success")
 		return redirect(url_for('login'))
@@ -127,35 +115,28 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		# get form fields
-		username = request.form['username']
+		username = request.form['username']												# get form fields
 		password_entered = request.form['password']
-		# create a cursor 
-		cur = mysql.connection.cursor()
-		# get username in db
-		result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
-		if result > 0: #if there is any rows
-			# get stored hash / dictionary configuration
-			data = cur.fetchone()
+		cur = mysql.connection.cursor()													# create a cursor 
+		result = cur.execute("SELECT * FROM users WHERE username = %s", [username])		# get username in db
+		if result > 0: 																	# if there are any rows:
+			data = cur.fetchone()														# get stored hash / dictionary configuration
 			password_db = data['password']
-			# compare hashed passwords
-			if sha256_crypt.verify(password_entered, password_db):
-				# passed
-				session["logged_in"] = True
+			if sha256_crypt.verify(password_entered, password_db):						# compare hashed passwords
+				session["logged_in"] = True												# passed code block
 				session["username"] = username
 				flash("~ GRANTED ACCESS ~", "success")
 				return redirect(url_for('dashboard'))
-			else:
+			else:																		# not passed code block
 				error = "~ PASSWORD INCORRECT ~"
 				return render_template('login.html', error=error)
-			# close connection 
-			cur.close()
+			cur.close()																	# close connection 
 		else:
 			error = "~ USERNAME INVALID ~"
 			return render_template('login.html', error=error)
 	return render_template('login.html')
 
-# logout
+# logout page aka a redirect to the login page
 @app.route('/logout')
 @is_logged_in
 def logout():
@@ -163,12 +144,16 @@ def logout():
 	flash("~ YOU'VE BEEN LOGGED OUT ~", "success")
 	return redirect(url_for('login'))
 
-# article form class
+# article class
 class ArticleForm(Form):
 	title = StringField('Title', [validators.Length(min=1, max=200)])
 	body = TextAreaField('Body', [validators.Length(min=30)])
 
-# add article
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# insertion(), edit(), delete() ~ std database operations below:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# add article ~ insertion()
 @app.route('/add_article', methods=['GET', 'POST'])
 @is_logged_in
 def add_article():
@@ -176,67 +161,51 @@ def add_article():
 	if request.method == 'POST' and form.validate():
 		title = form.title.data
 		body = form.body.data
-		# create cursor
 		cur = mysql.connection.cursor()
-		# execute 
 		cur.execute("INSERT INTO articles(title, body, author) VALUES(%s,%s,%s)", (title, body, session['username']))
-		# commit 
 		mysql.connection.commit()
-		# close / flash
 		cur.close()
 		flash("~ FILE UPLOADED ~", "success")
 		return redirect(url_for('dashboard'))
 	return render_template('add_article.html', form=form)
 
-# edit article
+# edit article ~ edit()
 @app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
 def edit_article(id):
-	# create cursor
 	cur = mysql.connection.cursor()
-	# get the article by respective 'id'
 	result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
 	article = cur.fetchone()
 	cur.close()
-	# get correct form
 	form = ArticleForm(request.form)
-	# populate article form fields
 	form.title.data = article['title']
 	form.body.data = article['body']
 	if request.method == 'POST' and form.validate():
 		title = request.form['title']
 		body = request.form['body']
-		# create cursor
 		cur = mysql.connection.cursor()
 		app.logger.info(title)
-		# execute 
 		cur.execute("UPDATE articles SET title = %s, body = %s WHERE id = %s", (title, body, id))
-		# commit 
 		mysql.connection.commit()
-		# close / flash
 		flash("~ FILE UPDATED ~", "success")
 		cur.close()
 		return redirect(url_for('dashboard'))
 	return render_template('edit_article.html', form=form)
 
-# delete article
+# delete article ~ delete()
 @app.route("/delete_article/<string:id>", methods=['POST'])
 @is_logged_in
-def delete_article(id):
-	# create cursor 
+def delete_article(id): 
 	cur = mysql.connection.cursor()
-	# execute
 	cur.execute("DELETE FROM articles WHERE id = %s", [id])
 	result = cur.execute("SELECT * FROM articles WHERE author = %s", [session["username"]])
-	# commit
 	mysql.connection.commit()
-	if result > 0: # aesthetics
+	if result > 0: 																					# aesthetics
 		flash("~ FILE DELETED ~", "success")
-	# close / flash
 	cur.close()
 	return redirect(url_for('dashboard'))
 
-# program starts here
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	  MAIN
 if __name__ == '__main__':
 	app.secret_key='**************'
 	app.run(debug=True)
