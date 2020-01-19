0b03bebfe78bed67c86f172140275e30f7821cd6
Kamran C <theblizzzzard@gmail.com>
2020-01-18 20:57:00 -0500

    Update fitnessGrapher.py

--- fitnessGrapher.py
+++ fitnessGrapher.py
@@ -1,7 +1,7 @@
 # Fitness Understanding Tool - V1
 # Currently deployed @ https://health-grapher.herokuapp.com/ (Please wait ~ 30 seconds to load)
 # Daily steps, heart rate, distance travelled, and caloric output were some of the metrics explored
-# Full stack application available upon request (Fitbit JSON Requests)
+# Full stack application available upon request (including Fitbit JSON Requests)
 # ~~~
 # Developer: Kamran Choudhry
 
